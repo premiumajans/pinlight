bdd14a76d325906b54415e56dadc3706e4dcca16
Elgiz Ismayilov <92743270+elgiz20002000@users.noreply.github.com>
2023-07-13 10:18:18 +0400

    first push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  i18n,
+  images: {
+    domains: ["admin.pinlight.az"],
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,75 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss'
+import type {AppProps} from 'next/app'
+import MobileMenu from "@/Components/MobileMenu/MobileMenu";
+import SearchWraper from "@/Components/SearchWraper/SearchWraper";
+import BackToTop from "@/Components/BackToTop/BackToTop";
+import Footer from "@/Components/Footer/Footer";
+import Header from "@/Components/Header/Header";
+import {StrictMode, useEffect, useState} from "react";
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+import {appWithTranslation} from "next-i18next";
+import {useRouter} from "next/router";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({Component, pageProps}: AppProps) {
+    const [loading, setLoading] = useState(false)
+    const [visible, setVisible] = useState(false)
+    const router = useRouter()
+
+    useEffect(() => {
+        AOS.init();
+        if (typeof window !== undefined) {
+            if (window.screen.width < 992) {
+                setVisible(true)
+            }
+        }
+    }, [])
+
+
+    useEffect(() => {
+        const handleRouteChange = () => {
+            setLoading(true)
+        };
+
+        const handleRouteComplete = () => {
+            setLoading(false)
+        };
+
+        router.events.on('routeChangeStart', handleRouteChange)
+        router.events.on('routeChangeComplete', handleRouteComplete)// If the component is unmounted, unsubscribe
+
+        return () => {
+            router.events.off('routeChangeStart', handleRouteChange)
+        }
+    }, [router])
+
+
+    return <>
+
+        <Header/>
+        <StrictMode>
+            <Component {...pageProps}/>
+        </StrictMode>
+        <Footer/>
+
+        <BackToTop/>
+
+        <SearchWraper/>
+        {visible ? <MobileMenu/> : ''}
+        {loading ? <div className="page-preloader">
+            <div className="loader">
+                <div></div>
+                <div></div>
+            </div>
+        </div> : ''}
+    </>
 }
+
+
+export default appWithTranslation(App);
+
+
+
+
+
+
